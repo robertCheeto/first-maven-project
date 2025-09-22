c9afe12f066de928939f3e33d74b3b7bddeeedea
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-09-22 14:15:39 -0400

    first commit of ShoppingList

---
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Bananas");
+        System.out.println("Orange Juice");
+        System.out.println("Greek Yogurt");
+        System.out.println("Chicken Breast");
+        System.out.println("Potatoes");
+        System.out.println("Broccoli");
+        System.out.println("Salami");
+        System.out.println("Bread");
+        System.out.println("Lemonade");
+        System.out.println("Ice Cream");
+    }
+}
